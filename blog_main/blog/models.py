e049e18a0d6c24637540e387abf3e554822e54f2
201611072 박승훈 <psh970718@naver.com>
2022-04-21 23:23:45 +0900

    Category 모델 생성

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -6,6 +6,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
